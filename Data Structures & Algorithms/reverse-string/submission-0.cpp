16d1a95c114432541790cc9f3c3b0fae426c2629
flavin-dev <flavinml07@gmail.com>
2026-07-24 19:53:54 +0530

    Update: reverse-string - submission-0

--- Data Structures & Algorithms/reverse-string/submission-0.cpp
+++ Data Structures & Algorithms/reverse-string/submission-0.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l=0,r=s.size()-1;
-        while(l<r){
-            char temp=s[l];
-            s[l]=s[r];
-            s[r]=temp;
-            l++;
-            r--;
+        int i=0,j=s.size()-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
         }
-        return;
     }
 };
