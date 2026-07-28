78ba7eb8695f11c841f74cd235a63f5198753235
flavin-dev <flavinml07@gmail.com>
2026-07-28 19:12:56 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int l=0,r=people.size()-1,cnt=0;
+        while(l<=r){
+            if(people[l]+people[r]<=limit){
+                l++;
+                r--;
+            }
+            else{
+                r--;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+};
